7c45f8a31fb3847bdf11b43ca6b23fc03bca3893
Habeeb <habeebahmadu1@gmail.com>
2024-12-03 22:46:25 +0100

    feat: add theme switcher

--- apps/web/components/index.ts
+++ apps/web/components/index.ts
@@ -1,5 +1,6 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import ThemeProvider from "@/components/providers/theme-provider";
+import { ThemeSwitcher } from "@/components/theme-switcher";
 
-export { Footer, Header, ThemeProvider };
+export { Footer, Header, ThemeProvider, ThemeSwitcher };
