f8b9352af17b8b4e5bee96131e5982c71b688f09
Habeeb <habeebahmadu1@gmail.com>
2024-12-04 20:43:28 +0100

    feat: add navigation menu component

--- apps/web/components/header.tsx
+++ apps/web/components/header.tsx
@@ -3,3 +3,7 @@ const Header = () => {
 };
 
 export default Header;
+
+// GET_SOCKET_URL: (trail_id: string, auth_token: string) => {
+//     return `https://${process.env.NEXT_PUBLIC_BACKEND_SERVICE}/socket/message-feed/${trail_id}/ws?auth_token=${auth_token}`;
+//   },
